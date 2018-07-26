7fc30fd94e80078ba10249758951ba2e73d8b40b
Mathew Gaudin <36637483+Matthew-Gaudin@users.noreply.github.com>
2018-07-26 00:53:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var msg = 'Click tongue to get lost in the Sllawce';
+function updateMessage() {
+    
+   var el = document.getElementById('message');
+   el .textContent = msg;}
+   updateMessage();
